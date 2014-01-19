0b12136377fd4e9b9e824c3ade7cb7aa44d70936
vanquangtoknow <vanquangtb101@gmail.com>
2014-01-18 22:59:17 -0800

    avav

---
+++ Source/ActionBar/src/info/androidhive/actionbar/LoginActivity.java
@@ -0,0 +1,17 @@
+package info.androidhive.actionbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+	public LoginActivity() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}
